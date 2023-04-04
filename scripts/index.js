cb70758c33bcd6d216ceeafe777e775d485f48a5
Tati Matveeva <renki.t@yandex.ru>
2023-04-05 03:33:09 +0700

    feat:верстка, css и js

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+//переменные
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const profileElement = document.querySelector('.profile__info');
+const inputName = profileElement.querySelector('.profile__title');
+const inputJob = profileElement.querySelector('.profile__subtitle');
+let inputNameNew = popupElement.querySelector('.popup__input_type-name');
+let inputJobNew = popupElement.querySelector('.popup__input_type-occupation');
+const formButtomSubmit = document.querySelector('.popup__button-submit');
+//открыли
+function openPopup () {
+  inputNameNew.value = inputName.textContent;
+  inputJobNew.value = inputJob.textContent;
+  popupElement.classList.add('popup_is-opened');
+};
+popupOpenButtonElement.addEventListener('click', openPopup);
+
+//закрыли
+function closePopup () {
+  popupElement.classList.remove('popup_is-opened');
+};
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+//сохранение и новые данные
+
+function popupSubmitButtonElement (evt) {
+  evt.preventDefault();
+  inputName.textContent = inputNameNew.value;
+  inputJob.textContent = inputJobNew.value;
+  closePopup ();
+};
+
+formButtomSubmit.addEventListener('click', popupSubmitButtonElement);
