6f80961d17dbe98cc195928ea00f6e6e938e44e9
Tati Matveeva <renki.t@yandex.ru>
2023-04-08 13:18:56 +0700

    fix: исправить ошибки ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,20 @@ const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const profileElement = document.querySelector('.profile__info');
 const inputName = profileElement.querySelector('.profile__title');
 const inputJob = profileElement.querySelector('.profile__subtitle');
-let inputNameNew = popupElement.querySelector('.popup__input_type-name');
-let inputJobNew = popupElement.querySelector('.popup__input_type-occupation');
+let inputNameNew = popupElement.querySelector('.popup__input_type_name');
+let inputJobNew = popupElement.querySelector('.popup__input_type_occupation');
 const formButtomSubmit = document.querySelector('.popup__button-submit');
 //открыли
 function openPopup () {
   inputNameNew.value = inputName.textContent;
   inputJobNew.value = inputJob.textContent;
-  popupElement.classList.add('popup_is-opened');
+  popupElement.classList.add('popup_opened');
 };
 popupOpenButtonElement.addEventListener('click', openPopup);
 
 //закрыли
 function closePopup () {
-  popupElement.classList.remove('popup_is-opened');
+  popupElement.classList.remove('popup_opened');
 };
 popupCloseButtonElement.addEventListener('click', closePopup);
 
