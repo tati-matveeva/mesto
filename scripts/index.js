570431f9023231550df2ecef6f40ad7a3b839020
Tati Matveeva <renki.t@yandex.ru>
2023-05-14 01:19:34 +0700

    style: ревью

--- scripts/index.js
+++ scripts/index.js
@@ -32,34 +32,6 @@ const submitAddElement = popupAddElement.querySelector('.popup__button-submit');
 const inputAddForm = popupAddElement.querySelectorAll('.popup__input');
 
 
-//массив для карточек 
-const initialCards = [
-  {
-    name: 'Челябинская область',
-    link: 'https://images.unsplash.com/photo-1661154649880-a11d195ecacc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80'
-  },
-  {
-    name: 'Териберка',
-    link: 'https://images.unsplash.com/photo-1606841002936-38996d5eea7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-  },
-  {
-    name: 'Санкт-Петербург',
-    link: 'https://images.unsplash.com/photo-1615116280262-8ad0321e70cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
-  },
-  {
-    name: 'Мыс Хамелеон',
-    link: 'https://images.unsplash.com/photo-1623617105388-ec22d118cfc5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
-  },
-  {
-    name: 'Владивосток',
-    link: 'https://images.unsplash.com/photo-1643281237869-90f896c8fd6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://images.unsplash.com/photo-1614093576028-920b30d65326?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80'
-  }
-];
-
 //открытие попап
 function openPopup (popup){
   popup.classList.add('popup_opened');
@@ -131,21 +103,21 @@ profileAddButtonElement.addEventListener('click', () => {
 })
 
 //карточки
-function newCard(object){
+function newCard(cardData){
   const cardElement = cardTemplate.querySelector('.elements__element').cloneNode(true);
   const imageElement = cardElement.querySelector('.elements__image');
   const deleteElement = cardElement.querySelector('.elements__delete-button');
   const likeElement = cardElement.querySelector('.elements__like-button');
-  imageElement.alt = object.name;
-  imageElement.src = object.link;
-  cardElement.querySelector('.elements__name').textContent = object.name;
+  imageElement.alt = cardData.name;
+  imageElement.src = cardData.link;
+  cardElement.querySelector('.elements__name').textContent = cardData.name;
   likeElement.addEventListener('click', (evt) => evt.target.classList.toggle('elements__like-button_active'));
   deleteElement.addEventListener('click', (evt) => evt.target.closest('.elements__element').remove());
 
   imageElement.addEventListener('click', () => {
-    popupImageElement.src = object.link;
-    popupImageElement.alt = object.name;
-    imagePopupCaption.textContent = object.name;
+    popupImageElement.src = cardData.link;
+    popupImageElement.alt = cardData.name;
+    imagePopupCaption.textContent = cardData.name;
     openPopup(imagePopupElement);
   })
   return cardElement;
